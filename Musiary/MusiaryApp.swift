e23aeb442c0207f4eddd88ca6a37ecd8ef0d0fe1
eomchanu <djacksdn1@icloud.com>
2024-04-11 16:44:43 +0900

    🎉 Feat: 프로젝트 생성
    gitignore 포함

---
+++ Musiary/MusiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusiaryApp.swift
+//  Musiary
+//
+//  Created by Eom Chanwoo on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MusiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
